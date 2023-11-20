88d816175664ac8ed6021fa7742dc3255ed86ee2
Ana Clara Heinzen <107196396+anaheinzen@users.noreply.github.com>
2023-11-20 09:39:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
 
-document.querySelector('#som_tecla_pom').play();
-
-
-
+const ListaDeTeclas = document.querySelectorAll('.tecla')
 
